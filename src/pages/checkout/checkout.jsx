59efd75394152896e1077f500c74916a672ff1e2
mohtareq <mtareq571@gmail.com>
2021-01-30 02:34:24 +0200

    18th

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -36,11 +36,6 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <TotalContainer>TOTAL: ${total}</TotalContainer>
-    <WarningContainer>
-      *Please use the following test credit card for payments*
-      <br />
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-    </WarningContainer>
     <StripeCheckoutButton price={total} />
   </CheckoutPageContainer>
 );
