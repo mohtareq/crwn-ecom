f560e1f9c0f7425f9ad42fdd46f52f388417ba18
hamzaaldirawi <hamza.dirawie@gmail.com>
2021-03-04 02:54:47 +0200

    Sagas Done

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import ShopPage from './pages/shop/shop.jsx';
 import Header from './components/header/header.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx';
 import CheckoutPage from "./pages/checkout/checkout.jsx";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import { setCurrentUser } from './redux/user/user.action';
 import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends React.Component {
@@ -17,9 +15,7 @@ class App extends React.Component {
 
   componentDidMount() {
 
-    const {setCurrentUser} = this.props;
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+   /* this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
@@ -30,8 +26,8 @@ class App extends React.Component {
           });
         });
       }
-      setCurrentUser(userAuth);
-    });
+      setCurrentUser(userAuth); 
+    });*/
   }
 
   componentWillUnmount() {
@@ -67,8 +63,4 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
